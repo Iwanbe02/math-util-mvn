d70be395db92f26f0017990602d72c959e76292c
Iwanbe02 <toancx202@gmail.com>
2023-06-27 14:35:54 +0700

    add screenshots; update source using recursion to see green4ever

--- src/main/java/org/Iwanbe02/mathutil/core/MathUtility.java
+++ src/main/java/org/Iwanbe02/mathutil/core/MathUtility.java
@@ -10,22 +10,45 @@ package org.iwanbe02.mathutil.core;
  */
 public class MathUtility {
 
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid n, n must be between 0...20, please");
+//        }
+//
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//
+//        return product;
+//    }
+    
     public static long getFactorial(int n) {
 
-        if (n < 0 || n > 20) {
+        if (n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid n, n must be between 0...20, please");
-        }
+        
 
-        if (n == 0 || n == 1) {
+        if (n == 0 || n == 1) 
             return 1;
-        }
+        
+        return  n * getFactorial(n - 1);
 
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-
-        return product;
+        
     }
 
 }
+
+//5! = 1 * 2 * 3 * 4 * 5 = 1 * 2 * 3 * 4   * 5
+//                       =    4!           * 5 = 5 * 4!
+
+//4! = 4 * 3!
+//3! = 3 * 2!
+//2! = 2 * 1!
+//1! = 1 DỪNG QUÁ TRÌNH ĐUỔI NHAU
+//N! = N * (N - 1)!
